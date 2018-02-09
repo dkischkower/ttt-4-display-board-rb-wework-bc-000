7fa215a34bf747270128b9ad8fecf973738be452
David Kischkower <david.kischkower@wework.com>
2018-02-09 03:52:49 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@
 
 def display_board(board)
   puts "    |   |   "
-  puts "-----------"
+  puts "-------------"
   puts "    |   |   "
-  puts "-----------"
+  puts "-------------"
   puts "    |   |   "
 end
 
